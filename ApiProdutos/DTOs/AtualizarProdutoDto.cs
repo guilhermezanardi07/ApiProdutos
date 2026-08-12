be4cda7abd4b9146836f368c73611c71a9f6eb63
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-08-12 09:11:27 -0300

    Edição no arquivo 'AtualizarProdutoDto.cs': Adicionado código que determina quais valores poderão ser atualizados.

--- ApiProdutos/DTOs/AtualizarProdutoDto.cs
+++ ApiProdutos/DTOs/AtualizarProdutoDto.cs
@@ -1,6 +1,13 @@
-﻿namespace ApiProdutos.DTOs;
-
+﻿using System.ComponentModel.DataAnnotations;
+namespace ApiProdutos.DTOs;
 public class AtualizarProdutoDto
 {
-    
-}
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
+    public string Nome { get; set; } = string.Empty;
+    [StringLength(500)]
+    public string Descricao { get; set; } = string.Empty;
+    [Range(0.01, 999999.99)]
+    public decimal Preco { get; set; }
+    [Range(0, int.MaxValue)]
+    public int QuantidadeEstoque { get; set; }
