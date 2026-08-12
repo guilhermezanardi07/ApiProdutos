a8ca638bc38cc0c2e9463af113d15ac0d189516f
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-08-12 09:09:02 -0300

    Edição em 'CriarProdutoDto.cs': Adição de códigos que determinam o formato e tipo de cada dado da classe.

--- ApiProdutos/DTOs/CriarProdutoDto.cs
+++ ApiProdutos/DTOs/CriarProdutoDto.cs
@@ -1,6 +1,14 @@
-﻿namespace ApiProdutos.DTOs;
-
+﻿using System.ComponentModel.DataAnnotations;
+namespace ApiProdutos.DTOs;
 public class CriarProdutoDto
 {
-    
+    [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+    [StringLength(100, MinimumLength = 3)]
+    public string Nome { get; set; } = string.Empty;
+    [StringLength(500)]
+    public string Descricao { get; set; } = string.Empty;
+    [Range(0.01, 999999.99)]
+    public decimal Preco { get; set; }
+    [Range(0, int.MaxValue)]
+    public int QuantidadeEstoque { get; set; }
 }
