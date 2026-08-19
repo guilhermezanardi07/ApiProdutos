e0a38735efee19def696f85336193ee1a738fe7c
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-08-19 08:38:32 -0300

    Alteração em 'ProdutosController.cs', permite que o controlador receba requisições em HTTP

--- ApiProdutos/Controllers/ProdutosController.cs
+++ ApiProdutos/Controllers/ProdutosController.cs
@@ -1,6 +1,34 @@
-﻿namespace ApiProdutos.Controllers;
+﻿using ApiProdutos.DTOs;
+using ApiProdutos.Models;
+using ApiProdutos.Repositories;
+using Microsoft.AspNetCore.Mvc;
+namespace ApiProdutos.Controllers;
 
-public class ProdutosController
+[ApiController]
+[Route("api/[controller]")]
+public class ProdutosController : ControllerBase
 {
-    
+    private readonly ProdutoRepository _repository;
+
+    public ProdutosController(ProdutoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>Retorna todos os produtos.</summary>
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> ListarTodos()
+    {
+        return Ok(_repository.ListarTodos());
+    }
+
+    /// <summary>Busca um produto por ID.</summary>
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> BuscarPorId(int id)
+    {
+        var produto = _repository.BuscarPorId(id);
+        if (produto is null)
+            return NotFound(new { mensagem = $"Produto com ID {id} não encontrado." });
+        return Ok(produto);
+    }
 }
