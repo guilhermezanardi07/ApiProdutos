05fae5620ce46b38436b86e235c98c2f79b608a1
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-08-19 08:21:41 -0300

    Atualização em 'Program.cs': Definindo como a aplicação irá funcionar.

--- ApiProdutos/Program.cs
+++ ApiProdutos/Program.cs
@@ -1,23 +1,34 @@
+using System.Reflection;
+using ApiProdutos.Repositories;
+using Microsoft.OpenApi;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddSingleton<ProdutoRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API de Produtos",
+        Version = "v1",
+        Description = "API REST para cadastro e gerenciamento de produtos."
+    });
+    var nomeXml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var caminhoXml = Path.Combine(AppContext.BaseDirectory, nomeXml);
+    if (File.Exists(caminhoXml))
+        options.IncludeXmlComments(caminhoXml);
+});
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json",
+            "API de Produtos v1");
+    });
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
